fd81a4a6655810d35e9a9fafed0a35e9c27cd4a2
Axel Fortunato <axel.fortunato.dev@gmail.com>
2026-08-27 12:36:14 +0200

    feat: add routing, navbar and static pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Weather } from './pages/weather/weather';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'weather/:city', component: Weather },
+  { path: 'about', component: About },
+  { path: '**', redirectTo: 'home' },
+];
